62a671bd447d98bf18f1cce3e28873a3c0f687f2
sh.tepsurkaev <shamil.tepsurkaev@simbirsoft.com>
2023-08-25 00:17:38 +0400

    add switch

---
+++ src/components/common/Switch/index.tsx
@@ -0,0 +1,47 @@
+import { useMemo } from 'react';
+import styles from 'components/common/Switch/styles.module.sass';
+
+type Props = {
+    modelValue?: boolean,
+    keyField: string,
+    children?: string,
+    updateValue?: (value: boolean) => void,
+}
+ 
+const defaultProps: Props = {
+    modelValue: false,
+    keyField: '',
+    children: 'Название радиокнопки',
+};
+
+function Switch(prevProps: Props): JSX.Element {
+    const props = { ...defaultProps, ...prevProps };
+
+    const updateValue = (value: boolean): void => {
+        props.updateValue?.(value);
+    };
+
+    const componentClasses = useMemo(() => {
+        return props.modelValue ? 'input_checked' : '';
+    }, [props.modelValue]);
+
+    return (
+        <div className={styles.switch}>
+            <div className={styles['switch__title']}>
+                {props.children}
+            </div>
+
+            <input
+                className={`${styles['switch__input']} ${styles[componentClasses]}`}
+                id={props.keyField}
+                type="checkbox"
+                checked={props.modelValue}
+                onChange={() => updateValue(!props.modelValue)}
+            />
+
+            <label htmlFor={props.keyField} />
+        </div>
+    );
+}
+
+export default Switch;
