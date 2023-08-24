61a29c4e83ffea70877c3d1dd8ddf58b877874fb
sh.tepsurkaev <shamil.tepsurkaev@simbirsoft.com>
2023-08-24 18:07:46 +0400

    create playground

--- src/components/common/Playground/index.tsx
+++ src/components/common/Playground/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import Input from 'components/common/Input';
+import React, { useState, useRef, useMemo } from 'react';
 import { ReactComponent as IconBulb } from 'assets/icons/bulb.svg';
 import { ReactComponent as IconGithub } from 'assets/icons/github.svg';
 import { ReactComponent as IconCode } from 'assets/icons/code.svg';
+import { ReactComponent as IconResize } from 'assets/icons/resize.svg';
 import styles from 'components/common/Playground/styles.module.sass';
 
 type InputChangeEvent = React.ChangeEvent<HTMLInputElement>;
 
 type Props = {
     className?: string,
+    codeTemplate?: string,
     gitLink?: string,
+    children?: React.ReactNode,
     onInput?: (event: InputChangeEvent) => void,
 }
  
 const defaultProps: Props = {
+    codeTemplate: '',
     gitLink: '',
 };
 
@@ -21,13 +24,50 @@ function Playground(prevProps: Props): JSX.Element {
     const props = { ...defaultProps, ...prevProps };
 
     // BLOCK "code show"
+    const [isCodeShow, setCodeShow] = useState<boolean>(false);
+
+    const codeClasses = useMemo(() => {
+        return isCodeShow ? 'code_open' : '';
+    }, [isCodeShow]);
+
     const showCode = (): void => {
-        // isCodeShow.value = !isCodeShow.value;
+        setCodeShow(!isCodeShow);
     };
     
     // BLOCK "field theme change"
+    const [isLight, setLight] = useState<boolean>(false);
+
     const setTheme = (): void => {
-        // isLight.value = !isLight.value;
+        setLight(!isLight);
+    };
+
+    const fieldClasses = useMemo(() => {
+        return isLight ? 'field-dark' : '';
+    }, [isLight]);
+
+    // BLOCK "field resize"
+    const refField = useRef<HTMLInputElement>(null);
+
+    const drag = (e: React.MouseEvent): void => {
+        let dragX = e.clientX;
+    
+        document.onmousemove = function onMouseMove(e: MouseEvent): void {
+            if (!refField.current) {
+                return;
+            }
+    
+            const maxWidthField = 768;
+            const newWidth = refField.current.offsetWidth + e.clientX - dragX;
+    
+            if (newWidth < maxWidthField) {
+                refField.current.style.width = newWidth + 'px';
+                dragX = e.clientX;
+            }
+        };
+    
+        document.onmouseup = function onMouseLeave(): void {
+            document.onmousemove = document.onmouseup = null;
+        };
     };
 
     return (
@@ -36,8 +76,6 @@ function Playground(prevProps: Props): JSX.Element {
                 Playground
 
                 <div className={styles['header-icons']}>
-                    Playground
-
                     <IconBulb
                         className={styles['icon-bulb']}
                         onClick={setTheme}
@@ -54,6 +92,27 @@ function Playground(prevProps: Props): JSX.Element {
                         onClick={showCode}
                     />
                 </div>
+            </div>
+
+            <div
+                className={`${styles.field} ${styles[fieldClasses]}`}
+                ref={refField}
+            >
+                {props.children}
+
+                <div className={styles['field-resize']}>
+                    <IconResize
+                        className={styles['icon-resize']}
+                        onMouseDown={drag}
+                    />
+                </div>
+            </div>
+
+            <div className={`${styles.code} ${styles[codeClasses]}`}>
+                <div
+                    className={styles['code-template']}
+                    dangerouslySetInnerHTML={{ __html: props.codeTemplate || '' }}
+                />
             </div>
         </div>
     );
